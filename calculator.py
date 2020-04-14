1ffb2a381ee771324b96a261c03619215101cc52
iTzBoboCz <bobo12002@icloud.com>
2020-04-14 17:31:55 +0200

    Dodělání tlačítek, předpřipravená kontrola vložených hodnot, předpřipravení funkcí

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-#from lib.py import *
+from lib import *
 from tkinter import *
 import json
 import os
@@ -27,7 +27,10 @@ class App:
         self.frameGrid()
 
         self.inputValue = StringVar()
-        self.input = Entry(self.frame, bg="#efbfef", textvariable=self.inputValue, justify='right')
+
+        # https://stackoverflow.com/a/4140988
+        inputValidate = self.root.register(self.onValidate)
+        self.input = Entry(self.frame, bg="#8395a7", textvariable=self.inputValue, validate="key", justify='right', validatecommand=(inputValidate, "%P"))
         self.input.grid(row=0, column=0, sticky=N+S+E+W, padx=5, pady=5, columnspan=4)
 
         self.buttonsFrame = Frame(self.frame, bg="#fff")
@@ -36,6 +39,12 @@ class App:
 
         self.createButtons()
         self.buttonsGrid()
+
+    def onValidate(self, value):
+        if re.search("[^\^0-9,/*%+\-√()]+", value):
+            return(False)
+        else:
+            return(True)
 
     def frameGrid(self):
         for x in range(4):
@@ -49,6 +58,11 @@ class App:
         for y in range(6):
             Grid.rowconfigure(self.buttonsFrame, y, weight=1)
 
+    def getButton(self, symbol, row, column, name):
+        functionCall = eval("CommandList."+name)
+        button = Button(self.buttonsFrame, text=symbol, font=("Arial", 14, "bold"), cursor="hand2", bd=0, bg="#4a69bd", activebackground="#82ccdd", fg="#ecf0f1", command= lambda: functionCall(self.inputValue)).grid(row=row, column=column, sticky=N+S+E+W)
+        return(button)
+
     def createButtons(self):
         # otevření json souboru s tlačítky
         try:
@@ -56,11 +70,10 @@ class App:
         except:
             print("[ERROR] Nebyl nalezen soubor s tlačítky - buttons.json.\nUkončuji program.")
             exit()
-
         buttonsData = json.load(buttonsJSON)
         for button in buttonsData:
             data = buttonsData[button]
-            self.buttons.append(Button(self.buttonsFrame, text=data["symbol"], font=("Arial", 14, "bold"), cursor="hand2", bd=0, bg="#4a69bd", activebackground="#82ccdd", fg="#ecf0f1").grid(row=data["row"], column=data["column"], sticky=N+S+E+W))
+            self.buttons.append(self.getButton(data["symbol"], data["row"], data["column"], button))
 
 if __name__ == '__main__':
     calculator = App(root)
---
+++ lib.py
@@ -0,0 +1,59 @@
+import re
+
+class CommandList():
+    def check(inputValue):
+        value = inputValue.get()
+        if inputValue is not None:
+            return(inputValue)
+        else:
+            retun(True)
+    def percentage(inputValue):
+        value = CommandList.check(inputValue)+"%"
+        re.sub("(0-9)$", "", value)
+        inputValue.set(value)
+    def nthRoot(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def secondPower(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def inversion(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def clearEntry(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def clear(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def delete(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def divide(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def multiply(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def minus(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def plus(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def plusMinusInversion(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def decimalSeparator(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def result(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_nine(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_eight(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_seven(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_six(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_five(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_four(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_three(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_two(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_one(inputValue):
+        inputValue.set(CommandList.check(inputValue))
+    def number_zero(inputValue):
+        inputValue.set(CommandList.check(inputValue))
